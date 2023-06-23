6d539011a5465f7b54d1208f8c3adfe3f292607c
Erik Rekola <erik.rekola@digia.com>
2023-06-23 10:50:26 +0300

    Replace api url with env variable

--- packages/client/src/hooks/useSocket.ts
+++ packages/client/src/hooks/useSocket.ts
@@ -3,9 +3,9 @@ import { io, Socket } from "socket.io-client";
 
 const useSocket = (events: { [key: string]: (...args: any[]) => void }) => {
   const socketRef = useRef<Socket>();
-
   useEffect(() => {
-    socketRef.current = io("ws://localhost:8080", {});
+    socketRef.current = io();
+
     Object.entries(events).forEach(([key, value]) => {
       socketRef.current!.on(key, value);
     });
